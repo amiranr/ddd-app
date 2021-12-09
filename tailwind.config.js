901375a3a638303275e80b8b73c1b3e97cfc38f0
amiranr <nuramira381@gmail.com>
2021-12-09 12:07:19 +0800

    restructured the codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}, 
   },
   variants: {
     extend: {},
